c47f3b54d4e186820f88f637b3fdcac23c432b0a
Zewin <0558568986m@gmail.com>
2022-12-09 18:05:14 +0200

    SingleChildScrollView in setting

--- lib/langauge_Theme_BottomSheet/bottomSheetLangauge.dart
+++ lib/langauge_Theme_BottomSheet/bottomSheetLangauge.dart
@@ -13,81 +13,83 @@ class bottomSheetLangauge extends StatelessWidget {
     var provider = Provider.of<MyProvider>(context);
     return Padding(
       padding: const EdgeInsets.all(25.0),
-      child: Column(
-        children: [
-          InkWell(
-            onTap: () {
-              swith = !swith;
-              provider.changeLanguage('en');
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: provider.languageLocal == "ar"
-                    ? Colors.grey
-                    : MyThemeData.colorGold,
-              ),
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.english,
-                    style: Theme.of(context)
-                        .textTheme
-                        .subtitle1
-                        ?.copyWith(fontSize: 40, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          InkWell(
-            onTap: () {
-              swith = !swith;
-              provider.changeLanguage('ar');
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: provider.languageLocal == "ar"
-                    ? MyThemeData.colorGold
-                    : Colors.grey,
-              ),
-              width: double.infinity,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.arabic,
-                    style: Theme.of(context)
-                        .textTheme
-                        .subtitle1
-                        ?.copyWith(fontSize: 40, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            height: 70,
-            width: 150,
-            child: CloseButton(
-              onPressed: () {
-                Navigator.pop(context);
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                swith = !swith;
+                provider.changeLanguage('en');
               },
-              color: Colors.amber,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: provider.languageLocal == "ar"
+                      ? Colors.grey
+                      : MyThemeData.colorGold,
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.english,
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle1
+                          ?.copyWith(fontSize: 40, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 50,
+            ),
+            InkWell(
+              onTap: () {
+                swith = !swith;
+                provider.changeLanguage('ar');
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: provider.languageLocal == "ar"
+                      ? MyThemeData.colorGold
+                      : Colors.grey,
+                ),
+                width: double.infinity,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.arabic,
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle1
+                          ?.copyWith(fontSize: 40, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 70,
+              width: 150,
+              child: CloseButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                color: Colors.amber,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/langauge_Theme_BottomSheet/bottomSheetTheme.dart
+++ lib/langauge_Theme_BottomSheet/bottomSheetTheme.dart
@@ -10,77 +10,79 @@ class bottomSheetTheme extends StatelessWidget {
     var provider = Provider.of<MyProvider>(context);
     return Padding(
       padding: const EdgeInsets.all(25.0),
-      child: Column(
-        children: [
-          InkWell(
-            onTap: () {
-              provider.changeThemeMode(ThemeMode.light);
-              provider.change();
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: provider.mode == ThemeMode.light
-                    ? MyThemeData.colorGold
-                    : Colors.grey,
-              ),
-              width: double.infinity,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(AppLocalizations.of(context)!.light,
-                      style: Theme.of(context)
-                          .textTheme
-                          .subtitle1
-                          ?.copyWith(fontSize: 40, color: Colors.white)),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          InkWell(
-            onTap: () {
-              provider.changeThemeMode(ThemeMode.dark);
-              provider.change();
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: provider.mode == ThemeMode.dark
-                    ? MyThemeData.colorGold
-                    : Colors.grey,
-              ),
-              width: double.infinity,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(AppLocalizations.of(context)!.dark,
-                      style: Theme.of(context)
-                          .textTheme
-                          .subtitle1
-                          ?.copyWith(fontSize: 40, color: Colors.white)),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Container(
-            height: 70,
-            width: 150,
-            child: CloseButton(
-              onPressed: () {
-                Navigator.pop(context);
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                provider.changeThemeMode(ThemeMode.light);
+                provider.change();
               },
-              color: Colors.amber,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: provider.mode == ThemeMode.light
+                      ? MyThemeData.colorGold
+                      : Colors.grey,
+                ),
+                width: double.infinity,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(AppLocalizations.of(context)!.light,
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle1
+                            ?.copyWith(fontSize: 40, color: Colors.white)),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 50,
+            ),
+            InkWell(
+              onTap: () {
+                provider.changeThemeMode(ThemeMode.dark);
+                provider.change();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: provider.mode == ThemeMode.dark
+                      ? MyThemeData.colorGold
+                      : Colors.grey,
+                ),
+                width: double.infinity,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(AppLocalizations.of(context)!.dark,
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle1
+                            ?.copyWith(fontSize: 40, color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 70,
+              width: 150,
+              child: CloseButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                color: Colors.amber,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
